0591bbd3b9d43b07213fe6abe4fe4adf00700d84
Eka Singgih <singgih11@gmail.com>
2018-12-15 15:56:05 +0700

    initial commit

---
+++ SMTPTester.FullFx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace SMTPTester.FullFx
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Tuple<bool, MailAddress> validation;
+
+            do
+            {
+                Console.Write("To: ");
+                var input = Console.ReadLine();
+                validation = IsValidEmail(input);
+            } while (!validation.Item1);
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Send(new MailMessage
+                {
+                    To = { validation.Item2 },
+                    Subject = "SMTP Tester",
+                    Body = "SMTP well configured"
+                });
+            }
+        }
+
+        private static Tuple<bool, MailAddress> IsValidEmail(string email)
+        {
+            try
+            {
+                var address = new MailAddress(email);
+                return new Tuple<bool, MailAddress>(address.Address == email, address);
+            }
+            catch
+            {
+                return new Tuple<bool, MailAddress>(false, null);
+            }
+        }
+    }
+}
